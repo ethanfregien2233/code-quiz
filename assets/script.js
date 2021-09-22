3f1cfd040964fd78c305ab5345dceddbca17efa7
ethan fregien <ethanfregien@gmail.com>
2021-09-22 17:32:22 -0500

    started js

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+var questions = [{
+    question: "Commonly used data types do NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "1"
+},
+{
+    question: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+    answer: "3"
+},
+{
+    question: "Arrays in Javascript can be used to store ____.",
+    choices: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: "4"
+},
+{
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    choices: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    answer: "3"
+},
+{
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    choices: ["1. Javascript", "2. terminal/bash", "3. for loops", "4. console.log"],
+    answer: "4"
+}
+
+]
